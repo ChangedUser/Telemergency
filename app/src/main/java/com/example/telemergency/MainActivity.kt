5f908214914b858686c1f4e10d921cec90b839cc
Marko <zivojinovic.ma@gmail.com>
2022-02-19 01:33:15 +0100

    WebRTC impelmentation push

--- app/src/main/java/com/example/telemergency/MainActivity.kt
+++ app/src/main/java/com/example/telemergency/MainActivity.kt
@@ -1,6 +1,14 @@
-package com.example.telemergency
+// package com.example.telemergency
+// package com.developerspace.telemedicine
 
-import android.os.Bundle
+package com.example.telemergency
+// import com.example.telemergency.ui.webrtc.Constants
+// import com.example.telemergency.ui.webrtc.RTCActivity
+
+// import com.developerspace.ui.webrtc.Constants
+// import com.developerspace.ui.webrtc.RTCActivity
+
+/* import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -50,5 +58,61 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
+ */
+
+// package com.developerspace.webrtcsample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.telemergency.ui.webrtc.Constants
+import com.example.telemergency.ui.webrtc.RTCActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_start.*
+
+class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+        Constants.isIntiatedNow = true
+        Constants.isCallEnded = true
+        start_meeting.setOnClickListener {
+            if (meeting_id.text.toString().trim().isNullOrEmpty()) {
+                meeting_id.error = "Please enter meeting id"
+            } else {
+                db.collection("calls")
+                    .document(meeting_id.text.toString())
+                    .get()
+                    .addOnSuccessListener {
+                        if (it["type"]=="OFFER" || it["type"]=="ANSWER" || it["type"]=="END_CALL") {
+                            meeting_id.error = "Please enter another meeting ID"
+                        } else {
+                            val intent = Intent(this@MainActivity, RTCActivity::class.java)
+                            intent.putExtra("meetingID",meeting_id.text.toString())
+                            intent.putExtra("isJoin",false)
+                            startActivity(intent)
+                        }
+                    }
+                    .addOnFailureListener {
+                        meeting_id.error = "Please enter a new meeting ID"
+                    }
+            }
+        }
+        join_meeting.setOnClickListener {
+            if (meeting_id.text.toString().trim().isNullOrEmpty())
+                meeting_id.error = "Please enter meeting id"
+            else {
+                val intent = Intent(this@MainActivity, RTCActivity::class.java)
+                intent.putExtra("meetingID",meeting_id.text.toString())
+                intent.putExtra("isJoin",true)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/telemergency/ui/webrtc/Constants.kt
@@ -0,0 +1,11 @@
+// package com.developerspace.webrtcsample
+// package com.developerspace.ui.webrtc
+// package com.example.telemergency.ui.webrtc
+package com.example.telemergency.ui.webrtc
+
+class Constants {
+    companion object {
+        var isCallEnded: Boolean = false
+        var isIntiatedNow : Boolean = true
+    }
+}
