5a909857e2f8d533272cd4919c93c968040e28b5
unknown <tmoci@yahoo.com>
2022-02-21 20:31:06 +0100

    user roles

--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.kotlinmessenger
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_main.*
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     //private lateinit var mDbRef: DatabaseReference
+    var radioGroup: RadioGroup? = null
+    var radioButton: RadioButton? = null
+    var role = "Patient";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        radioGroup = findViewById(R.id.radioGroup);
         FirebaseMessaging.getInstance().subscribeToTopic("bpmAlert")
         register_button_register.setOnClickListener {
             val email = email_edittext_registration.text.toString()
@@ -49,7 +55,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun saveUserToFirebaseDatabase(uid : String){
-        val user = User(uid, username_edittext_registration.text.toString())
+        val user = User(uid, username_edittext_registration.text.toString(), role)
         val mDbRef = FirebaseDatabase.getInstance("https://telemedizinproject-default-rtdb.europe-west1.firebasedatabase.app/").getReference("/users/$uid")
         mDbRef.setValue(user)
             .addOnSuccessListener {
@@ -63,9 +69,15 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Failed to register user: ${it.message}", Toast.LENGTH_LONG).show()
             }
     }
+
+    fun checkButton(v: View?) {
+        val radioId = radioGroup!!.checkedRadioButtonId
+        radioButton = findViewById(radioId)
+        role = radioButton?.getText() as String
+    }
 }
 
 @Parcelize
-class User(val uid : String, val username : String): Parcelable {
-    constructor(): this("", "")
+class User(val uid : String, val username : String, val role : String): Parcelable {
+    constructor(): this("", "", "")
 }
